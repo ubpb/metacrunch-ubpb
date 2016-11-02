c19bc10bb93142552c8dc4f8ea53369900544233
Michael Sievers <michael_sievers@web.de>
2016-11-02 13:00:28 +0100

    Do not replace title_sort with short_title_sort
    - ubpb/issues#80

--- lib/metacrunch/ubpb/transformations/primo_to_elasticsearch/replace_title_sort_with_short_title_sort.rb
+++
@@ -1,9 +0,0 @@
-require "metacrunch/hash"
-require "metacrunch/transformator/transformation/step"
-require_relative "../primo_to_elasticsearch"
-
-class Metacrunch::UBPB::Transformations::PrimoToElasticsearch::ReplaceTitleSortWithShortTitleSort < Metacrunch::Transformator::Transformation::Step
-  def call
-    target["title_sort"] = target.delete("short_title_sort")
-  end
-end
