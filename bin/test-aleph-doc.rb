b77eefd80af249c370aede84b5717be497f154d4
René Sprotte <renspr@gmail.com>
2017-06-30 10:35:32 +0200

    Add a command line tool to test the transformations against any record from the Aleph production system.

---
+++ bin/test-aleph-doc.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+require "bundler/setup"
+require "metacrunch/ubpb"
+
+require "open-uri"
+begin require "pry" rescue LoadError ; end
+
+record_id      = ARGV[0]
+aleph_base_url = ENV["aleph_base_url"] || "http://ubex.uni-paderborn.de:1891"
+url            = "#{aleph_base_url}/rest-dlf/record/PAD01#{record_id}?view=full"
+data           = nil
+mab2primo      = Metacrunch::UBPB::Transformations::MabToPrimo.new
+primo2es       = Metacrunch::UBPB::Transformations::PrimoToElasticsearch.new
+
+def decode_json!(object)
+  case object
+  when Array
+    object.map! do |_value|
+      decode_json!(_value)
+    end
+  when Hash
+    object.each do |_key, _value|
+      object[_key] = decode_json!(_value)
+    end
+  when String
+    if object.start_with?("{") && object.end_with?("}")
+      JSON.parse(object)
+    else
+      object
+    end
+  else
+    object
+  end
+end
+
+open(url) { |io| data = io.read }
+
+result = mab2primo.call(data)
+decode_json!(result)
+result = primo2es.call(result)
+
+puts JSON.pretty_generate(result)
