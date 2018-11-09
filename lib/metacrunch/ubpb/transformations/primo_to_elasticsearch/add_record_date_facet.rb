2bb34239952efb59ef9ecf3b009c7e586d02feed
René Sprotte <renspr@gmail.com>
2018-11-09 11:03:16 +0100

    Fix error handling for cataloging_date

--- lib/metacrunch/ubpb/transformations/primo_to_elasticsearch/add_record_date_facet.rb
+++ lib/metacrunch/ubpb/transformations/primo_to_elasticsearch/add_record_date_facet.rb
@@ -5,8 +5,11 @@ require_relative "../primo_to_elasticsearch"
 class Metacrunch::UBPB::Transformations::PrimoToElasticsearch::AddCatalogingDate < Metacrunch::Transformator::Transformation::Step
   def call
     if (last_creation_date = [source["creation_date"]].flatten.compact.last)
-      if cataloging_date = Date.strptime(last_creation_date, "%Y%m%d").iso8601 rescue ArgumentError
-        target["cataloging_date"] = cataloging_date
+      begin
+        if cataloging_date = Date.strptime(last_creation_date, "%Y%m%d").iso8601
+          target["cataloging_date"] = cataloging_date
+        end
+      rescue ArgumentError
       end
     end
   end
